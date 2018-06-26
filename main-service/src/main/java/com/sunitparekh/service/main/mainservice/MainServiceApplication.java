cea4e1c9cf3c9e6a6b7ff518e52e0f55ec22c932
sunitparekh <parekh.sunit@gmail.com>
2018-06-26 11:53:52 +0530

    main service

---
+++ main-service/src/main/java/com/sunitparekh/service/main/mainservice/MainServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sunitparekh.service.main.mainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainServiceApplication.class, args);
+	}
+}

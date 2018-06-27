2f4b53e9b5b53e228eaa0bf63f100b5937302208
sunitparekh <parekh.sunit@gmail.com>
2018-06-27 12:11:48 +0530

    main service calling 2 other services

---
+++ main-service/src/main/java/com/sunitparekh/service/main/mainservice/HelloWorldController.java
@@ -0,0 +1,41 @@
+package com.sunitparekh.service.main.mainservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+@RestController
+public class HelloWorldController {
+
+    @Value("${app.service.1.host}")
+    String serviceName1;
+
+    @Value("${app.service.2.host}")
+    String serviceName2;
+
+    @Autowired
+    RestTemplate template;
+
+    @GetMapping("/hello")
+    public Map<String, String> hello(@RequestParam String name) {
+        Logger.getLogger(HelloWorldController.class.getName()).info("************* MainService: HelloWorldController");
+        Map result = new HashMap();
+        try {
+            result.putAll(Objects.requireNonNull(template.getForObject("http://" + serviceName1 + "/hello/" + name, Map.class)));
+            result.putAll(Objects.requireNonNull(template.getForObject("http://" + serviceName2 + "/hello/" + name, Map.class)));
+        } catch (Exception e) {
+            Logger.getLogger(HelloWorldController.class.getName()).severe(e.getMessage());
+        }
+        result.put("mainService", "hello " + name);
+        return result;
+    }
+}
--- main-service/src/main/java/com/sunitparekh/service/main/mainservice/MainServiceApplication.java
+++ main-service/src/main/java/com/sunitparekh/service/main/mainservice/MainServiceApplication.java
@@ -2,9 +2,18 @@ package com.sunitparekh.service.main.mainservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@Configuration
 public class MainServiceApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainServiceApplication.class, args);
